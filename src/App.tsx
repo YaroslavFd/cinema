fcd71d4cfb89d6c00dabc0a194abed724c7994a7
Yaroslav <yaroslav.web9@gmail.com>
2023-07-10 16:52:40 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const App = () => (
+  <div>
+    <h2 className="test">App</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, doloribus provident magnam
+      doloremque consequatur amet ex maiores minima enim inventore nulla eos quis ab aliquam hic
+      voluptate eum iusto repudiandae.
+    </p>
+  </div>
+)
